1e0ea04c33bc97733ec27fcb078f67bf07798e3d
NubeDev <aidan@enviroservices.com.au>
2020-06-29 19:53:35 +1000

    Added in IO cal in py

---
+++ calibration.py
@@ -0,0 +1,12 @@
+from func import command_to_bool, is_float, analog_in, analog_out, digital_in, digital_out
+
+print(is_float)
+# rawReads = {
+#     "UI1": ui1Raw,
+#     "UI2": ui2Raw,
+#     "UI3": ui3Raw,
+#     "UI4": ui4Raw,
+#     "UI5": ui5Raw,
+#     "UI6": ui6Raw,
+#     "UI7": ui7Raw,
+# }
